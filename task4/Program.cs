9f3cfc76572570d926353ef6e4b9f78616853905
Julia_Lvova <juliasmirnova3@mail.ru>
2022-10-16 16:28:03 +0300

    added code

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("333Hello, World!");
+﻿//  Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2 3 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+
+int a = 25;
+int b = 27;
+int c = 26;
+
+if (a > b && a > c)
+{
+    Console.WriteLine(a);
+}
+else if (b > a && b > c)   
+{
+    Console.WriteLine(b);
+}
+else if (c > a && c > b)  
+
+Console.WriteLine(c);
+
+    
